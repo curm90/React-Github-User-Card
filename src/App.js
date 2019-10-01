c282069dea839d72ed414ee4f9d69dce4dc34d1e
Liam Sutton <devliam90@gmail.com>
2019-10-01 12:39:56 +0100

    add axios request and store user data in state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return <div className='App'></div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: []
+    };
+  }
+
+  componentDidMount() {
+    Axios.get('https://api.github.com/users/curm90')
+      .then(res => {
+        this.setState({ user: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <p>App Component</p>
+      </div>
+    );
+  }
 }
 
 export default App;
